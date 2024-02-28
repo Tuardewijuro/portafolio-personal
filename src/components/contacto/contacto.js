d3e70ffd01f7ecc773fd9736fd108d05d88839f3
Tuardewijuro <99750572+Tuardewijuro@users.noreply.github.com>
2024-02-28 11:39:13 -0500

    Add react-icons and styling changes

--- src/components/contacto/contacto.js
+++ src/components/contacto/contacto.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Navbar from '../navbar';
 import Footer from '../footer';
+import Recuadro from '../recuadro';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import './contacto.css';
 
 const Contacto = () => (
-    <div className="min-h-screen bg-gradient-background flex flex-col">
+    <div className="flex flex-col min-h-screen bg-gradient-background">
         <Navbar />
-        <h1 className="text-4xl text-center mt-6 font-bold text-green-500">
-            Contacto
-        </h1>
+        <div className="flex-grow flex flex-col items-center justify-center">
+            <Recuadro title="Contacto">
+                <div className="flex space-x-8 mt-6">
+                    <a href="mailto:tuemail@gmail.com" target="_blank" rel="noreferrer">
+                        <FaEnvelope size={90} color="white" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/tu-usuario/" target="_blank" rel="noreferrer">
+                        <FaLinkedin size={90} color="white" />
+                    </a>
+                    <a href="https://github.com/tu-usuario" target="_blank" rel="noreferrer">
+                        <FaGithub size={90} color="white" />
+                    </a>
+                </div>
+            </Recuadro>
+        </div>
         <Footer />
     </div>
 );
--- src/components/recuadro.js
+++ src/components/recuadro.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './recuadro.css';
 
-const Recuadro = ({ title, children }) => (
-    <div className="p-6 max-w-sm mx-auto bg-black text-white rounded-xl shadow-md flex items-center space-x-4 flex-col recuadro" style={{ width: '500px' }}>
+const Recuadro = ({ title, children, conBorde }) => (
+    <div className={`p-6 max-w-sm mx-auto bg-black text-white rounded-xl shadow-md flex items-center space-x-4 flex-col ${conBorde ? 'recuadro' : ''}`} style={{ width: '500px' }}>
         <div className="text-xl font-bold text-center w-full">{title}</div>
         <div className="mt-4 w-full flex-grow flex flex-col items-center">
             {children}
